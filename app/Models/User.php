331c00685bc8981b4348803ae0dd2bb7a916d855
Laravel Daily <info@laraveldaily.com>
2021-02-25 14:10:46 +0000

    QuickAdminPanel automatic commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\VerifyUserNotification;
 use Carbon\Carbon;
 use Hash;
 use Illuminate\Auth\Notifications\ResetPassword;
@@ -9,6 +10,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use \DateTimeInterface;
 
 class User extends Authenticatable
@@ -24,6 +26,7 @@ class User extends Authenticatable
 
     protected $dates = [
         'email_verified_at',
+        'verified_at',
         'created_at',
         'updated_at',
         'deleted_at',
@@ -33,6 +36,9 @@ class User extends Authenticatable
         'name',
         'email',
         'email_verified_at',
+        'verified',
+        'verified_at',
+        'verification_token',
         'password',
         'remember_token',
         'created_at',
@@ -50,6 +56,37 @@ class User extends Authenticatable
         return $this->roles()->where('id', 1)->exists();
     }
 
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        self::created(function (User $user) {
+            if (auth()->check()) {
+                $user->verified    = 1;
+                $user->verified_at = Carbon::now()->format(config('panel.date_format') . ' ' . config('panel.time_format'));
+                $user->save();
+            } elseif (!$user->verification_token) {
+                $token     = Str::random(64);
+                $usedToken = User::where('verification_token', $token)->first();
+
+                while ($usedToken) {
+                    $token     = Str::random(64);
+                    $usedToken = User::where('verification_token', $token)->first();
+                }
+
+                $user->verification_token = $token;
+                $user->save();
+
+                $registrationRole = config('panel.registration_default_role');
+
+                if (!$user->roles()->get()->contains($registrationRole)) {
+                    $user->roles()->attach($registrationRole);
+                }
+
+                $user->notify(new VerifyUserNotification($user));
+            }
+        });
+    }
+
     public function getEmailVerifiedAtAttribute($value)
     {
         return $value ? Carbon::createFromFormat('Y-m-d H:i:s', $value)->format(config('panel.date_format') . ' ' . config('panel.time_format')) : null;
@@ -58,6 +95,16 @@ class User extends Authenticatable
     public function setEmailVerifiedAtAttribute($value)
     {
         $this->attributes['email_verified_at'] = $value ? Carbon::createFromFormat(config('panel.date_format') . ' ' . config('panel.time_format'), $value)->format('Y-m-d H:i:s') : null;
+    }
+
+    public function getVerifiedAtAttribute($value)
+    {
+        return $value ? Carbon::createFromFormat('Y-m-d H:i:s', $value)->format(config('panel.date_format') . ' ' . config('panel.time_format')) : null;
+    }
+
+    public function setVerifiedAtAttribute($value)
+    {
+        $this->attributes['verified_at'] = $value ? Carbon::createFromFormat(config('panel.date_format') . ' ' . config('panel.time_format'), $value)->format('Y-m-d H:i:s') : null;
     }
 
     public function setPasswordAttribute($input)
