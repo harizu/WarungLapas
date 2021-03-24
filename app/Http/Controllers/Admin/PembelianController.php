f7ee1afa8189db5134e7e6bc1da34d7b6e074dc9
Abdul Haris <haris@logique.co.id>
2021-03-24 23:29:03 +0700

    add comma

--- app/Http/Controllers/Admin/PembelianController.php
+++ app/Http/Controllers/Admin/PembelianController.php
@@ -44,7 +44,7 @@ class PembelianController extends Controller
     public function postPembelian(PostPembelianRequest $request)
     {
         $data = $request->validated();
-        
+
         switch ($data['step']) {
             case 1:
                 session([
@@ -118,7 +118,7 @@ class PembelianController extends Controller
             return redirect()->route('admin.pembelians.success',[
                 'order_no'        => '#' . $order->order_no,
                 'total'           => $order->total_pembayaran,
-                'biaya_layanan'   => $order->biaya_layanan
+                'biaya_layanan'   => $order->biaya_layanan,
                 'rekening_bank'   => config('transaction.rekening.bank'),
                 'rekening_no'     => config('transaction.rekening.no'),
                 'rekening_name'   => config('transaction.rekening.atas_nama'),
